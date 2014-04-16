364e86582e8a8c35dda8dbc51bed8645d05729ab
Eric Stensland <estensland88@gmail.com>
2014-04-16 18:38:53 -0500

    refactored 5 js

--- 5 Smallest Multiple/problem5.js
+++ 5 Smallest Multiple/problem5.js
@@ -1,74 +1,24 @@
-var isDivAllToTwenty = function(number){
-  if (number === 0) {
-    return false;
-  }
-  if (number % 2 !== 0) {
+var isDivTo = function(number, breakPoint){
+  divisor = breakPoint
+  while(divisor > breakPoint/2){
+    if (number % divisor !== 0) {
       return false;
+    } 
+    divisor --;
   }
-  if (number % 3 !== 0) {
-      return false;
-  }
-  if (number % 4 !== 0) {
-      return false;
-  }
-  if (number % 5 !== 0) {
-      return false;
-  }
-  if (number % 6 !== 0) {
-      return false;
-  }
-  if (number % 7 !== 0) {
-      return false;
-  }
-  if (number % 8 !== 0) {
-      return false;
-  }
-  if (number % 9 !== 0) {
-      return false;
-  }
-  if (number % 11 !== 0) {
-      return false;
-  }
-  if (number % 13 !== 0) {
-      return false;
-  }
-  if (number % 14 !== 0) {
-      return false;
-  }
-  if (number % 15 !== 0) {
-      return false;
-  }
-  if (number % 16 !== 0) {
-      return false;
-  }
-  if (number % 17 !== 0) {
-      return false;
-  }
-  if (number % 18 !== 0) {
-      return false;
-  }
-  if (number % 19 !== 0) {
-      return false;
-  }
-  console.log(number % 19)
   return true;
 };
 
-var firstDivByAll = function() {
+var firstDivByAll = function(number) {
   found = false;
-  i = 0;
-  while (found === false && i <= 232792580) {
-    if (isDivAllToTwenty(i)) {
+  i = 20;
+  while (found === false) {
+    if (isDivTo(i, number)) {
       found = true;
-      console.log("wooo")
-      console.log(i)
-      return i
+      return i;
     }
     i = i + 20;
   }
-  return i
 };
 
-
-
-console.log(firstDivByAll());
+console.log(firstDivByAll(20));
