19ada64034075d9e6a1ef493d53ffa43f4f6ffdc
Eric Stensland <estensland88@gmail.com>
2014-04-16 12:25:22 -0500

    finished number 2 in js

---
+++ 2 Even Fibonacci numbers/problem2.js
@@ -0,0 +1,18 @@
+
+
+var current = 2;
+var prev = 1;
+
+var even_fibs = [current];
+
+while(current < 4000000) {
+  next = current + prev;
+  
+  if (next % 2 === 0){even_fibs.push(next);}
+  prev = current;
+  current = next;
+}
+
+sum = even_fibs.reduce(function(a,b){return a+b;});
+
+console.log(sum);
