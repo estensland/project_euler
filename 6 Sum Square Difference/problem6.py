34bfface8e58b5bb1d3e77d0b849fdc658207c4d
Eric Stensland <estensland88@gmail.com>
2014-04-16 19:13:25 -0500

    finished 1 with python

---
+++ 6 Sum Square Difference/problem6.py
@@ -0,0 +1,13 @@
+import math
+
+def squared_sum_to(number):
+  regular_sum = 0
+  squared_sum = 0
+
+  for x in range (1, number + 1):
+    regular_sum = regular_sum + x
+    squared_sum = squared_sum + math.pow(x,2)
+
+  return math.pow(regular_sum,2) - squared_sum
+  
+print squared_sum_to(100)
