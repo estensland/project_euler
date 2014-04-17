148a93701a74714befc0c9c01809238ee449024b
Eric Stensland <estensland88@gmail.com>
2014-04-16 19:32:36 -0500

    refactoring ruby pallindromes

--- 4 Largest Palindrome Product/problem4.rb
+++ 4 Largest Palindrome Product/problem4.rb
@@ -10,23 +10,18 @@ def pallindrome_finder(low, high)
     until y > high
       z = x * y
       z = z.to_s
-      if z[0] == z[-1] && z[1] == z[-2]
-        if z.length < 5
-          pallindromes << z.to_i  
-        else
-          pallindromes << z.to_i if z[2] == z[-3]
-        end
+      if z == z.reverse
+        pallindromes << z.to_i  
       end
       y += 1
     end
     x+=1
   end
-
   pallindromes
 end
 
 def largest_pallindrome(low, high)
-  pallindrome_finder(low, high).length
+  pallindrome_finder(low, high).last
 end
 
 p largest_pallindrome(100, 999)
