0101ab3a3bbad3132a43ec1f4593848251ddb454
Eric Stensland <estensland88@gmail.com>
2014-09-18 21:58:25 -0500

    finished problem 10

---
+++ 10 Summation of Primes/10.js
@@ -0,0 +1,20 @@
+var primeCheck = function(number) {
+  var i = 2;
+  while (i < (Math.sqrt(number) + 1)) {
+    if (number % i === 0 && number !== 2) {return false;}
+    i ++;
+  }
+  return true;
+};
+
+var count = 2;
+var sum = 0;
+while (count < 2000000){
+  if (primeCheck(count)) {
+    sum = sum + count;
+    console.log(count)
+  }
+  count ++;
+}
+
+console.log(sum)
