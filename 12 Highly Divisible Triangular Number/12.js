24c5efb5097e21a3b13895ca6fa67fc3d438d1d4
Eric <estensland88@gmail.com>
2014-09-24 08:28:46 -0500

    updating `12

--- 12 Highly Divisible Triangular Number/12.js
+++ 12 Highly Divisible Triangular Number/12.js
@@ -1,17 +1,23 @@
 var base = 1;
 var i = 1;
+var divisorCounter = 0
 
-while(i < 100) {
+while(divisorCounter < 501) {
   base += i
   i += 1;
-  var holding = []
-  var divisor = base % 2 == 0 ? base / 2 : Math.round(base/3)
-  while(divisor > 0) {
-    if (base % divisor == 0) {holding.push(divisor)}
-    divisor -= 1 ;
+  var divisor = 1
+  divisorCounter = 0
+  while(divisor <= Math.sqrt(base)) {
+    if (base % divisor == 0) {
+      if (base/divisor !== divisor){
+        divisorCounter += 2
+      }
+      else{
+        divisorCounter += 1
+      }
+    }
+    divisor += 1 ;
   }
-  console.log(holding);
 }
 
 console.log(base);
-console.log(i);
