5799b1b6e3fa9913a9521385fa5f2105acf4c0fb
Eric <estensland88@gmail.com>
2014-09-30 14:00:06 -0500

    finished 18 in ruby

--- 18 Maximum path sum I/18.rb
+++ 18 Maximum path sum I/18.rb
@@ -14,11 +14,18 @@ triangle=           [[75],
  [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
 [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
 
-sum =[]
+triangle.reverse!
 
-triangle.reverse.each do |row|
-  max = row.max
-  index = sum.index(max)
-  sum += max
-
+triangle.each_with_index do |row, row_index|
+  next if row_index == 0
+  row.each_with_index do |cell, cell_index|
+    if triangle[row_index - 1][cell_index] > triangle[row_index - 1][cell_index + 1]
+      max = triangle[row_index - 1][cell_index]
+    else
+      max = triangle[row_index - 1][cell_index + 1]
+    end
+    triangle[row_index][cell_index] = cell + max
+  end
 end
+
+p triangle
