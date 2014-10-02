3b6867d99315b72df86e4fead499e5df33177566
Eric <estensland88@gmail.com>
2014-10-02 07:49:37 -0500

    attempt at 19, unclear if functional

--- 19 Counting Sundays/19.rb
+++ 19 Counting Sundays/19.rb
@@ -10,10 +10,16 @@ def days_in_year(num)
   end
 end
 
-total_number_of_days = 0
+total_number_of_monday = 0
+mondays = 0
+day_offset = 0
 
 1900.upto(2000) do |num|
-  total_number_of_days += days_in_year(num)
+  jan_days_mondays = 31 - day_offset
+  mondays += jan_days_mondays / 7
+  days_in_year(num)
+  day_offset += days_in_year(num) % 7
+  day_offset = day_offset > 6 ? day_offset : day_offset - 7
 end
 
-p total_number_of_days
+p mondays
