9a331d5992b8ac3691c59af3e4b9a96462f8b4dc
Eric <estensland88@gmail.com>
2014-10-02 07:50:53 -0500

    changes typo

--- 19 Counting Sundays/19.rb
+++ 19 Counting Sundays/19.rb
@@ -11,15 +11,15 @@ def days_in_year(num)
 end
 
 total_number_of_monday = 0
-mondays = 0
+sunday = 0
 day_offset = 0
 
 1900.upto(2000) do |num|
-  jan_days_mondays = 31 - day_offset
-  mondays += jan_days_mondays / 7
+  jan_days_sunday = 31 - day_offset
+  sunday += jan_days_sunday / 7
   days_in_year(num)
   day_offset += days_in_year(num) % 7
   day_offset = day_offset > 6 ? day_offset : day_offset - 7
 end
 
-p mondays
+p sunday
