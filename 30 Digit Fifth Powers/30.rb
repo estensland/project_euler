884afc00962a23df2b94f445810612c56a211aed
Eric Stensland <estensland88@gmail.com>
2014-11-23 10:25:02 -0600

    completed 30

--- 30 Digit Fifth Powers/30.rb
+++ 30 Digit Fifth Powers/30.rb
@@ -1,17 +1,21 @@
-def digit_fith_powers?(number)
+def digit_powers?(number, power)
   return false if number == 1 || number == 0
   array = number.to_s.split(//).map(&:to_i)
 
-  digit_sum = array.inject(0){|sum, i| sum += i ** 4}
+  digit_sum = array.inject(0){|sum, i| sum += i ** power}
   number == digit_sum
+end
+
+
+def digit_fith_powers?(number)
+  digit_powers?(number, 5 )
 end
 
 
 res = []
 
-(1000..10000000).to_a.each do |n|
-  next if n.odd?
+(2..1000000).to_a.each do |n|
   res << n if digit_fith_powers?(n)
 end
 
-p res
+p res.inject(:+)
