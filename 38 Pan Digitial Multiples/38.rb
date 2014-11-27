04321a44536e3e0eb6d1b6515bb38ccc222b4f3c
Eric Stensland <estensland88@gmail.com>
2014-11-27 09:49:44 -0600

    completed 38

--- 38 Pan Digitial Multiples/38.rb
+++ 38 Pan Digitial Multiples/38.rb
@@ -2,11 +2,29 @@ def number_split(num)
   num.to_s.split(//).map(&:to_i)
 end
 
-def pandigital?(num)
-  split = number_split(num)
+def pandigital?(split)
   split_uniq = split.uniq.reject(&:zero?)
   return false if split_uniq.length != split.length
   split_uniq.inject(:+) == 45
 end
 
-p pandigital?(198273645)
+res = []
+
+1.upto(1000) do |i|
+  split_num = number_split(i)
+
+  next if split_num.uniq.length != split_num.length
+
+  n = 2
+  holder = split_num
+
+  until n >= i || holder.length >= 9
+    holder += number_split(n * i)
+    n = i if pandigital?(holder)
+    n += 1
+  end
+
+  res << holder.join.to_i if pandigital?(holder)
+end
+
+p res.sort.reverse
