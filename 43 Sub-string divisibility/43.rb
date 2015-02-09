2d18da17b89796c90df3435ef587c297b4412dea
Eric Stensland <estensland88@gmail.com>
2015-02-08 21:07:41 -0600

    Update 43.rb

---
+++ 43 Sub-string divisibility/43.rb
@@ -0,0 +1,16 @@
+original = [1,2,3,4,5,6,7,8,9,0]
+
+nums = []
+
+original.permutation.each do |pandigital|
+  d = pandigital
+  return unless ((d[7] * d[8] * d[9]) % 17) == 0
+  return unless ((d[6] * d[7] * d[8]) % 13) == 0
+  return unless ((d[5] * d[6] * d[7]) % 11) == 0
+  return unless ((d[4] * d[5] * d[6]) % 7) == 0
+  return unless ((d[3] * d[4] * d[5]) % 5) == 0
+  return unless ((d[2] * d[3] * d[4]) % 3) == 0
+  return unless ((d[1] * d[2] * d[3]) % 2) == 0
+  nums << d
+end
+
