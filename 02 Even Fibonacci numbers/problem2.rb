f6225bf93261676d5a701213d1c8c5fce754061c
Eric Stensland <estensland88@gmail.com>
2015-02-13 11:16:27 -0600

    finished prob 2 in ruby

--- 02 Even Fibonacci numbers/problem2.rb
+++ 02 Even Fibonacci numbers/problem2.rb
@@ -1,1 +1,16 @@
- 
+def fibonacci_below(number)
+  prev = 0
+  fib = 1
+  nums = []
+
+ until fib >= number
+  store = fib
+  fib += prev
+  prev = store
+  nums << fib
+ end
+
+ nums
+end
+
+p fibonacci_below(4_000_000).select(&:even?).reduce(:+)
