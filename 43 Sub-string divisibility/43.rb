ddaf6a9f4c585db8678c9efcac91de1aaee2605b
Eric Stensland <estensland88@gmail.com>
2015-04-29 22:05:47 -0500

    completed 43 in ruby

--- 43 Sub-string divisibility/43.rb
+++ 43 Sub-string divisibility/43.rb
@@ -1,19 +1,17 @@
-original = [1,2,3,4,5,6,7,8,9,0]
+original = ['1','2','3','4','5','6','7','8','9','0']
 
 nums = []
 
 original.permutation.each do |pandigital|
   d = pandigital
-  return unless ((d[7] * d[8] * d[9]) % 17) == 0
-  return unless ((d[6] * d[7] * d[8]) % 13) == 0
-  return unless ((d[5] * d[6] * d[7]) % 11) == 0
-  return unless ((d[4] * d[5] * d[6]) % 7) == 0
-  return unless ((d[3] * d[4] * d[5]) % 5) == 0
-  return unless ((d[2] * d[3] * d[4]) % 3) == 0
-  return unless ((d[1] * d[2] * d[3]) % 2) == 0
-  nums << d.inject(:+)
+  next unless ((d[7] + d[8] + d[9]).to_i % 17) == 0
+  next unless ((d[6] + d[7] + d[8]).to_i % 13) == 0
+  next unless ((d[5] + d[6] + d[7]).to_i % 11) == 0
+  next unless ((d[4] + d[5] + d[6]).to_i % 7) == 0
+  next unless ((d[3] + d[4] + d[5]).to_i % 5) == 0
+  next unless ((d[2] + d[3] + d[4]).to_i % 3) == 0
+  next unless ((d[1] + d[2] + d[3]).to_i % 2) == 0
+  nums << d.join('')
 end
 
-nums << d.inject(:+)
-
-p nums
+p nums.map(&:to_i).inject(:+)
