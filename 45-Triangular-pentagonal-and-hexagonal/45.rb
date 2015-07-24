5c935a974d3b1e435b5d22528571e7788120210d
Eric Stensland <estensland88@gmail.com>
2015-07-24 18:10:52 -0500

    Update 45.rb

--- 45-Triangular-pentagonal-and-hexagonal/45.rb
+++ 45-Triangular-pentagonal-and-hexagonal/45.rb
@@ -1,9 +1,6 @@
-
-
-
 def triangle_num?(num)
-    res = (num * 8 + 1) ** 0.5
-    res % 1 ==0 && res % 2 != 0
+  res = (num * 8 + 1) ** 0.5
+  res % 1 ==0 && res % 2 != 0
 end
 
 def is_pentagon_number?(num)
@@ -12,8 +9,16 @@ def is_pentagon_number?(num)
   (top / 6) % 1 == 0
 end
 
-def is_hexagontal_number?(num)
-  top = 8 * num + 1
-  top = Math.sqrt(top) + 1
-  (top / 4) % 1 == 0
+def nth_hexagonal_number(num)
+  num * ((2 * num) -1)
+end
+
+i = 144
+
+until i > 100000
+  num = nth_hexagonal_number(i)
+  if triangle_num?(num) && is_pentagon_number?(num)
+      puts [num, i]
+  end
+  i += 1
 end
